e27c7c4c191a88873650321c561cc7888e10e271
Alex Prieto <alexprieto1997@gmail.com>
2024-07-03 16:20:11 -0500

    Add class FieldLayout in funcion ap_rcbd

--- FunctionsAP/statistics/__init__.py
+++ FunctionsAP/statistics/__init__.py
@@ -1,5 +1,4 @@
 # FunctionsAP/statistics/__init__.py
 
-from .ap_rcbd import ap_rcbd
-from .FieldLayout import FieldLayout
+from .ap_rcbd import ap_rcbd, FieldLayout
 from .modific_outlier import modific_outlier
