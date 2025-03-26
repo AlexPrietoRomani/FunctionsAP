27576839ea77a7217658049027fc06c13eb6ebeb
Alex Prieto <alexprieto1997@gmail.com>
2025-03-26 09:41:27 -0500

    agregate normalize_and_merge_columns
    se agrega la utilización de la función creada anteriormente en el archivo de join_files.

--- FunctionsAP/__init__.py
+++ FunctionsAP/__init__.py
@@ -1,5 +1,5 @@
 # FunctionsAP/__init__.py
-from .utilities import join_files, columns_add, eliminar_valor_columna1, procesar_valores, handle_missing_data
+from .utilities import join_files, columns_add, eliminar_valor_columna1, procesar_valores, handle_missing_data, normalize_and_merge_columns
 from .utilities.data_base_genetic import data_base_genetic
 from .statistics import diseño_rcbd, FieldLayout, modific_outlier
 from .graph import barplot_line_grouped_stacked
--- FunctionsAP/utilities/__init__.py
+++ FunctionsAP/utilities/__init__.py
@@ -2,7 +2,7 @@
 
 from .columns_add import columns_add
 from .eliminar_valor_columna1 import eliminar_valor_columna1
-from .join_files import join_files
+from .join_files import join_files, normalize_and_merge_columns
 from .procesar_valores import procesar_valores
 from .data_base_genetic import data_base_genetic
 from .handle_missing_data import handle_missing_data
